6fe16665c394e780cdc6909bfff9697a6f66129e
ArthurMeloG <arthurdemelo2001@gmail.com>
2022-07-09 19:54:01 -0300

    tela inicial

--- MiranhaPizza/App.js
+++ MiranhaPizza/App.js
@@ -1,13 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, Center, Box} from 'native-base';
+import { ScrollView } from 'react-native';
+import { NativeBaseProvider, Text, Center, Box, Image, HStack } from 'native-base';
 
 export default function App() {
   return (
-    <Box>
-      <Center>Ajude o Miranha a entregar pizzas</Center>
-      <StatusBar style="auto" />
-    </Box>
+    <NativeBaseProvider>
+      <Center marginTop={10} marginBottom={10}>Ajude o Miranha a entregar pizzas</Center>
+      <ScrollView horizontal>
+      <Box>
+        <HStack space={3} justifyContent="center">
+
+        <Box marginTop={100}>
+          <Image
+            size={100} 
+            resizeMode={"contain"} 
+            borderRadius={100} 
+            source={{uri: "https://wallpaperaccess.com/full/317501.jpg"}} 
+            alt="Alternate Text" 
+          />
+        </Box>
+        <Box marginTop={600}>
+          <Image 
+            size={100} 
+            resizeMode={"contain"} 
+            borderRadius={100} 
+            source={{uri: "https://wallpaperaccess.com/full/317501.jpg"}} 
+            alt="Alternate Text" 
+          />
+        </Box>
+        <Box marginTop={300}>
+          <Image 
+            size={100} 
+            resizeMode={"contain"} 
+            borderRadius={100} 
+            source={{uri: "https://wallpaperaccess.com/full/317501.jpg"}} 
+            alt="Alternate Text" 
+          />
+        </Box>
+        <Box marginTop={400}>
+          <Image 
+            size={100} 
+            resizeMode={"contain"} 
+            borderRadius={100} 
+            source={{uri: "https://wallpaperaccess.com/full/317501.jpg"}} 
+            alt="Alternate Text" 
+          />
+        </Box>
+        <Box>
+          <Image 
+            size={100} 
+            resizeMode={"contain"} 
+            borderRadius={100} 
+            source={{uri: "https://wallpaperaccess.com/full/317501.jpg"}} 
+            alt="Alternate Text" 
+          />
+        </Box>
+        <Box>
+          <Image 
+            size={100} 
+            resizeMode={"contain"} 
+            borderRadius={100} 
+            source={{uri: "https://wallpaperaccess.com/full/317501.jpg"}} 
+            alt="Alternate Text" 
+          />
+        </Box>
+        <Box>
+          <Image 
+            size={100} 
+            resizeMode={"contain"} 
+            borderRadius={100} 
+            source={{uri: "https://wallpaperaccess.com/full/317501.jpg"}} 
+            alt="Alternate Text" 
+          />
+        </Box>
+        <Box>
+          <Image 
+            size={100} 
+            resizeMode={"contain"} 
+            borderRadius={100} 
+            source={{uri: "https://wallpaperaccess.com/full/317501.jpg"}} 
+            alt="Alternate Text" 
+          />
+        </Box>
+        </HStack>
+      </Box>
+      </ScrollView>
+      
+    </NativeBaseProvider>
   );
 }
 
