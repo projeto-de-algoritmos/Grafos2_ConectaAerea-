3ee43fefe21e7c30f2d9df65c95f2fdec6893dc3
ArthurMeloG <arthurdemelo2001@gmail.com>
2022-07-09 10:08:47 -0300

    front incial

---
+++ MiranhaPizza/App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, Center, Box} from 'native-base';
+
+export default function App() {
+  return (
+    <Box>
+      <Center>Ajude o Miranha a entregar pizzas</Center>
+      <StatusBar style="auto" />
+    </Box>
+  );
+}
+
