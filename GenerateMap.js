7805b4deadd9a66064cae0e6cf54f8629e5989f0
luisgfmarques <luisgustavomarques@outlook.com>
2022-07-10 00:34:06 -0300

    include Dijask not fuction algoritm

--- GenerateMap.js
+++ GenerateMap.js
@@ -1,5 +1,5 @@
 let tamanho_do_mapa = 100
-let raio_teia = 2
+let raio_teia = 10
 
 function generate_int(max) {
     return Math.floor(Math.random() * (max - (max * -1) + 1)) + (max * -1);
@@ -14,7 +14,7 @@ function generate_city(square_size) {
     // Cordenadas {X,Y}
     var pos = []
     for (let i = 0; i < square_size; i++) {
-        pos.push([generate_int(square_size), generate_int(square_size)])
+        pos.push([generate_int(square_size), generate_int(square_size), i])
     }
     return pos
 }
@@ -24,23 +24,62 @@ function Gerar_grafo(build_pos, range_cobweb) {
     var graph = new Array(size)
     // Zerar matriz de adjacencia 
     for (let i = 0; i < size; i++) {
-        graph[i] = [0]
+        graph[i] = [Infinity]
         for (let k = 0; k < size - 1; k++) {
-            graph[i].push(0)
+            graph[i].push(Infinity)
         }
     }
     for (let i = 0; i < size; i++) {
         for (let k = 0; k < size; k++) {
-            if (calculate_distance(build_pos[i], build_pos[k]) < range_cobweb) {
-                graph[i][k] = calculate_distance(build_pos[i], build_pos[k])
+            if (calculate_distance(build_pos[i], build_pos[k]) <= range_cobweb && i != k) {
+                graph[i][k] = +(calculate_distance(build_pos[i], build_pos[k])).toFixed(4)
+            }
+            else {
+                graph[i][k] = Infinity
             }
         }
     }
 
     return graph
 }
-var city = generate_city(5)
+
+function Dijask(initial_index, final_index, graph) {
+    var priorityquery = []
+    var visited = []
+    var set = new Array(graph.length)
+    for (let i = 0; i < graph.length; i++) {
+        priorityquery.push([graph[initial_index][i], i, 0])
+        set[i] = [null, 0]
+    }
+    priorityquery.sort((a, b) => a[0] - b[0])
+    while (priorityquery.length > 0 && visited.length < graph.length) {
+        var current = priorityquery.shift()
+        if (current[0] != Infinity) {
+            if (set[current[1]][0] >= current[0]) {
+                set[current[1]] = [current[0], current[2]]
+                console.log(" entrou no set set ")
+            }
+            if (current[1] == final_index) {
+                return set
+            }
+            // visited.push(current[])
+            for (let i = 0; i < graph.length; i++) {
+                if (graph[current[1]][i] != Infinity) {
+                    priorityquery.push([graph[current[1]][i], i, 0])
+                }
+            }
+            visited.push(current[1])
+        }
+        priorityquery.sort((a, b) => a[0] - b[0])
+    }
+    return set
+}
+
+city = generate_city(10)
 console.log("Cordenadas das cidades")
 console.table(city)
 console.log("Distancia entre predios")
-console.table(Gerar_grafo(city, 3))
+graph = Gerar_grafo(city, raio_teia)
+console.table(Gerar_grafo(city, 10))
+console.log("Dijassk")
+console.table(Dijask(0, 5, graph))
